2acdbb3f1c276ce70ae29036bee6408c2daed2e6
zhangdaiping <zhangdaiping@gmail.com>
2014-06-24 18:42:22 +0800

    none

--- examples/wwdc-2014-rainbow-banner/main.js
+++ examples/wwdc-2014-rainbow-banner/main.js
@@ -100,7 +100,7 @@
     function flash(i, j) {
         setTimeout(function() {
             $pattern[0].children[i * col + j].className += ' flash';
-        }, i * 16.7 + j * 30);
+        }, i * 10 + j * 20);
     }
 
 
